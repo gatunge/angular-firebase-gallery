1e6c46269e4f33570080ef04261c841c0968ef6a
gatunge <gatungegerrald2@gmail.com>
2018-06-17 17:55:04 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ImageDetailComponent } from './image-detail/image-detail.component';
+import { UploadComponent } from './upload/upload.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    ImageDetailComponent,
+    UploadComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule
