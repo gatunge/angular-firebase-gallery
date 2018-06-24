734835be72e4ec17869d5a2c828c32527976a5bc
root <root@localhost.localdomain>
2018-06-24 17:36:55 +0200

    first commit

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ImageService } from '../services/image.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  constructor(private imageService: ImageService) { }
 
   ngOnInit() {
   }
